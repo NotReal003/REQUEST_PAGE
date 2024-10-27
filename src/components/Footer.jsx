c22df3727e1f403dc34e1be0e23e8c70209bde90
NotReal <117504040+NotReal003@users.noreply.github.com>
2024-10-27 09:23:05 +0000

    Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer class="bg-white dark:bg-gray-900">
-      <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+      <div class="mx-auto w-full p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
             <a href="https://notreal003.xyz/" class="flex items-center">
